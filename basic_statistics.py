376fad24d9a502ef6faa5b67401abe70416a9413
ldocao <ldocao@gmail.com>
2018-08-31 18:33:57 +0200

    feat: pairplot and unique values
    issue #1

--- basic_statistics.py
+++ basic_statistics.py
@@ -1,6 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 from revenue import Revenue
 
 
 revenue = Revenue().clean()
 revenue.info() 
-revenue.describe() 
+revenue.describe()
+for c in revenue.columns:
+    if revenue[c].dtype == np.dtype("O"):
+        print(c, revenue[c].unique())
+
+plt.figure()
+sns.pairplot(revenue)
+plt.tight_layout()
+plt.savefig("pairplot.png")
