a17e0269b088dc7948144a9dc55e31a0c2e709ca
ldocao <ldocao@gmail.com>
2018-09-01 11:57:25 +0200

    feat: plot also age histogram
    issue #8

--- study_couple.py
+++ study_couple.py
@@ -22,8 +22,21 @@ plt.hist(non_couple_revenue["Incomes"], color="b", **PLOT_OPTIONS)
 plt.hist(couple_revenue["Incomes"], color="r", **PLOT_OPTIONS)
 plt.xlabel("Incomes")
 plt.tight_layout()
-plt.savefig("study_couple_histogram.png")
+plt.savefig("study_couple_histogram_incomes.png")
 
 plt.figure()
 sns.jointplot(x="Incomes", y="Age", data=couple_revenue, kind="kde");
 plt.savefig("study_couple_jointplot.png")
+
+
+
+PLOT_OPTIONS = {"bins": 20,
+                "alpha": 0.3,
+                "normed": 1} #area should sum to 1
+
+plt.figure()
+plt.hist(non_couple_revenue["Age"], color="b", **PLOT_OPTIONS)
+plt.hist(couple_revenue["Age"], color="r", **PLOT_OPTIONS)
+plt.xlabel("Age")
+plt.tight_layout()
+plt.savefig("study_couple_histogram_age.png")
