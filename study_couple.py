a85b3910815becd5484c7befdeb01bdc860fee6d
ldocao <ldocao@gmail.com>
2018-09-01 11:39:48 +0200

    feat: simple histogram and kdeplot age vs incomes
    issue #8

---
+++ study_couple.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from revenue import Revenue
+
+revenue = Revenue().clean()
+is_couple = revenue["Sex"] == "Couple"
+couple_revenue = revenue[is_couple]
+non_couple_revenue = revenue[~is_couple]
+print(couple_revenue.describe())
+print(non_couple_revenue.describe())
+
+
+PLOT_OPTIONS = {"bins": np.arange(30e3, 200e3, 2e3),
+                "alpha": 0.3,
+                "normed": 1} #area should sum to 1
+plt.figure()
+plt.hist(non_couple_revenue["Incomes"], color="b", **PLOT_OPTIONS)
+plt.hist(couple_revenue["Incomes"], color="r", **PLOT_OPTIONS)
+plt.xlabel("Incomes")
+plt.tight_layout()
+plt.savefig("study_couple_histogram.png")
+
+plt.figure()
+sns.jointplot(x="Incomes", y="Age", data=couple_revenue, kind="kde");
+plt.savefig("study_couple_jointplot.png")
