33c86a0379d2e17719f9509e4a6f0f47ddd77b70
ldocao <ldocao@gmail.com>
2018-09-01 10:07:56 +0200

    feat: remove specific dates
    issue #2

--- revenue.py
+++ revenue.py
@@ -9,9 +9,15 @@ class Revenue:
 
 
     def clean(self):
+        self._remove_specific_dates()
         self._replace_date()
         return self.data
 
+    def _remove_specific_dates(self):
+        """Remove date that looks like XX/XX/XXXX, see issue #1"""
+        is_specific = self.data["Date"].str.contains("/", na=False)
+        self.data = self.data[~is_specific]
+        
 
     def _replace_date(self):
         self.data = self.data.rename(columns={"Date": "is_weekend"}) #inplace will be deprecated in pandas 1.0
