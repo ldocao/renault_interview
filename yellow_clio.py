94aa1d258a3eb022e398e80859480fddfc82c9fa
ldocao <ldocao@gmail.com>
2018-09-02 09:55:29 +0200

    feat: print count comparison on sex and is_weekend
    I normalized the value to 1 so that it's the percentage difference
    
    close #10

--- yellow_clio.py
+++ yellow_clio.py
@@ -17,7 +17,7 @@ median_yellow_clio = yellow_clio.groupby(["Sex", "is_weekend"]).median()
 
 
 
-
+# age and incomes
 comparison = median_all.merge(median_yellow_clio,
                               left_index=True,
                               right_index=True,
@@ -25,6 +25,18 @@ comparison = median_all.merge(median_yellow_clio,
 comparison["age_diff"] = comparison["Age_yc"] - comparison["Age_all"]
 comparison["incomes_diff"] = comparison["Incomes_yc"] - comparison["Incomes_all"]
 comparison = comparison.drop(["Age_all", "Incomes_all", "Age_yc", "Incomes_yc"], axis=1)
+
+
+
+# sex and is_weekend
+ARBITRARY_COLUMN = "Age"
+count_all = revenue.groupby(["Sex", "is_weekend"]).count()[ARBITRARY_COLUMN]
+count_yellow_clio = yellow_clio.groupby(["Sex", "is_weekend"]).count()[ARBITRARY_COLUMN]
+n_all = count_all.sum()
+n_yellow_clio = count_yellow_clio.sum()
+count_all /= n_all
+count_yellow_clio /= n_yellow_clio
+comparison_count = count_yellow_clio - count_all
 
 plt.figure()
 sns.pairplot(yellow_clio, hue="Sex")
