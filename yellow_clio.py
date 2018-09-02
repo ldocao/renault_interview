f5948b8d684fae2f8d1fec599abbea59ccb4e7e1
ldocao <ldocao@gmail.com>
2018-09-02 09:47:47 +0200

    feat: compare yellow clio clients to median customers across all cars
    close #10

--- yellow_clio.py
+++ yellow_clio.py
@@ -7,14 +7,24 @@ import seaborn as sns
 
 
 revenue = Revenue().clean()
-median_all = revenue.groupby(["Sex", "is_weekend"]).median()
+median_all = revenue.groupby(["Sex", "is_weekend"]).median()[["Age", "Incomes"]]
 
 is_clio = revenue["Model"] == "Clio"
 is_yellow = revenue["Color"] == "yellow"
 yellow_clio = revenue[is_clio & is_yellow]
 yellow_clio = yellow_clio.drop(["Model", "Color", "Price"], axis=1)
+median_yellow_clio = yellow_clio.groupby(["Sex", "is_weekend"]).median()
 
 
+
+
+comparison = median_all.merge(median_yellow_clio,
+                              left_index=True,
+                              right_index=True,
+                              suffixes=["_all", "_yc"])
+comparison["age_diff"] = comparison["Age_yc"] - comparison["Age_all"]
+comparison["incomes_diff"] = comparison["Incomes_yc"] - comparison["Incomes_all"]
+comparison = comparison.drop(["Age_all", "Incomes_all", "Age_yc", "Incomes_yc"], axis=1)
 
 plt.figure()
 sns.pairplot(yellow_clio, hue="Sex")
